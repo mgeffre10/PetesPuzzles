897b3e0b65bc407aff2317e90d70c2cc325b0043
Michael Geffre <geffrejunior92@gmail.com>
2020-05-01 14:52:32 -0700

    Adding new game mode to match project name

---
+++ Source/PuzzleRooms/PetesPuzzlesGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PetesPuzzlesGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEROOMS_API APetesPuzzlesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
